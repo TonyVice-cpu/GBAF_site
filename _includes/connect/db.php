6204d36831e85f323009bfcc0ee58a8ce9cee49f
Tony <tonyvice@hotmail.fr>
2023-01-24 01:03:32 +0100

    Ajout de commentaires -maj db.php

--- _includes/connect/db.php
+++ _includes/connect/db.php
@@ -1,7 +1,20 @@
 <?php
 
 /**
- * Connexion PDO
+ * Fichier de connexion PDO.
+ * Chargé à partir du fichier de fonctions ayant rapport avec l'utilisation de la base de données (functions.php)
+ * 
+ * Les connexions sont établies en créant des instances de la classe de base de PDO.
+ * Peu importe quel pilote vous voulez utiliser ; vous utilisez toujours le nom de la classe PDO.
+ * Le constructeur accepte des paramètres pour spécifier la source de la base de données (connue en tant que DSN) et optionnellement,
+ * le nom d'utilisateur et le mot de passe (s'il y en a un).
+ */
+
+/**
+ * Connexion à MySQL
+ * Adresse de la base de données
+ * Nom d'utilisateur MySQL
+ * Mdp MySQL
  */
 $dsn = 'mysql:dbname=gbaf;host=localhost';
 $user = 'root';
@@ -9,18 +22,27 @@ $pass = '';
 
 try {
   $bdd = new PDO($dsn, $user, $pass);
-  // On lui demande de configurer PDO::ATTR_ERRMODE qui va nous sortir un rapport ...
-  // ... d’erreurs et que l’on souhaite qu’il émette une exception grâce à PDO::ERRMODE_EXCEPTION.
+  /**
+   *On lui demande de configurer PDO::ATTR_ERRMODE qui va nous sortir un rapportd’erreurs
+   *et que l’on souhaite qu’il émette une exception grâce à PDO::ERRMODE_EXCEPTION.
+   */
   $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  // On récupère seulement le tableau associatif.
+  /** 
+   * On récupère seulement le tableau associatif.
+   */
   $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-  // Cette ligne permet la compatibilité ascendante php 8.1+
+  /**
+   * Cette ligne permet la compatibilité ascendante php 8.1+
+   */
   $bdd->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, true);
-  // Cette ligne a pour but de définir le type d'encodage à effectuer sur nos requêtes.
+  /** 
+   * Cette ligne a pour but de définir le type d'encodage à effectuer sur nos requêtes.
+   */
   $bdd->exec('SET NAMES utf8');
-  // Ici la connexion est effectuée.
 } catch (PDOException $e) {
-  // En cas d'échec de connexion, du par exemple à la non présence d'une base de données, PDO nous retounera alors un message d'erreur.
+  /** 
+   * En cas d'échec de connexion, du par exemple à la non présence d'une base de données, PDO nous retounera alors un message d'erreur.
+   */
   echo 'Échec lors de la connexion : ' . $e->getMessage();
   die;
 }
